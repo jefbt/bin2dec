f4f933c149f14e915371eabd08bfd2912825327a
Jeferson Tomazella <jefbt@outlook.com>
2020-02-18 00:10:02 -0300

    feat(input): receive input data up to 8 char

---
+++ src/router.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
+
+import paths from "./config/paths";
+
+import Home from "./pages/home";
+
+class Router extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path={paths.home}
+            component={Home}
+          />
+          <Route
+            path="*"
+            component={<div>Not Found!</div>}
+          />
+
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Router;
