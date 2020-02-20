d2964fbb75b59cfd13ae236c3e1592eac0058e80
Jeferson Tomazella <jefbt@outlook.com>
2020-02-20 16:33:31 -0300

    Updates

---
+++ precache-manifest.a4c01f194c908b33180bb8a55f5e112d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b543cfc04abfeaede0bdc42c7d487036",
+    "url": "/bin2dec/index.html"
+  },
+  {
+    "revision": "dec2868c9d11917be704",
+    "url": "/bin2dec/static/js/2.cc0cd7d0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/bin2dec/static/js/2.cc0cd7d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec463f8e5643955f5b6",
+    "url": "/bin2dec/static/js/main.6aa8301a.chunk.js"
+  },
+  {
+    "revision": "9badc5a01e82ba02df45",
+    "url": "/bin2dec/static/js/runtime-main.a710ecd5.js"
+  }
+]);
